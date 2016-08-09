1cf0c1990ce233a3c89e6badb13d6a355061c001
Bhavya <bhavsk9@gmail.com>
2016-08-09 15:53:11 -0700

    refactor commit - test 3

--- FareyGeneratorTest/UnitTest1.cs
+++ FareyGeneratorTest/UnitTest1.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+using System.Collections.Generic;
 
 namespace FareyGeneratorTest
 {
@@ -26,6 +26,14 @@ namespace FareyGeneratorTest
         {
             var result = FareyGeneratorLib.FareyGenerator.Generate(3);
             Assert.AreEqual(5, result.Count);
+        }
+
+        [TestMethod]
+        public void TestThreeDetails()
+        {
+            var result = FareyGeneratorLib.FareyGenerator.Generate(3);
+            var expected = new List<string>() { "0/1", "1/3", "1/2", "2/3", "1/1" };
+            CollectionAssert.AreEqual(expected, result);
         }
     }
 }
